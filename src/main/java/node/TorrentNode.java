09b888ecf9b1e61e337c14361779158f2682f4a6
Sergiu Breban <breban.sergiu94@gmail.com>
2018-01-30 00:04:37 +0200

    first commit

---
+++ src/main/java/node/TorrentNode.java
@@ -0,0 +1,12 @@
+package node;
+
+import java.util.logging.Logger;
+
+public class TorrentNode {
+
+  private static final Logger logger = Logger.getLogger(TorrentNode.class.getName());
+
+  public static void main(String[] args) {
+
+  }
+}
