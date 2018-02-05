735c6dc7eb4f1f7ad2bcb3cb6947c49b78ced054
Sergiu Breban <breban.sergiu94@gmail.com>
2018-02-05 20:20:54 +0200

    Read chunks

--- src/main/java/util/MessageUtil.java
+++ src/main/java/util/MessageUtil.java
@@ -15,16 +15,19 @@ public class MessageUtil {
   public static byte[] getMessageBytes(Socket socket) throws IOException {
     byte[] size = new byte[4];
     InputStream clientInputStream = new BufferedInputStream(socket.getInputStream());
-    int read = clientInputStream.read(size, 0, 4);
+    int bytesRead = clientInputStream.read(size, 0, 4);
     ByteBuffer wrapped = ByteBuffer.wrap(size); // big-endian by default
     int messageSize = wrapped.getInt();
-    byte[] buffer = new byte[messageSize];
-    logger.fine(socket + " " + messageSize + " " + read);
-    read = clientInputStream.read(buffer, 0, messageSize);
-    if (read == messageSize) {
-      return buffer;
+    int offset = 0;
+    byte[] data = new byte[messageSize];
+    while ((bytesRead = clientInputStream.read(data, offset, data.length - offset))
+            != -1) {
+      offset += bytesRead;
+      if (offset >= data.length) {
+        break;
+      }
     }
-    return null;
+    return data;
   }
 
   public static void sendMessage(Socket socket, Message message) throws IOException {
